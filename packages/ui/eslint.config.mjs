08e96a8c07630852ff4c3b2fe1666d434bcdc0b3
KunalNasa <kunalnasa.dev@gmail.com>
2025-05-13 23:35:52 +0530

    update package name and added initial folders

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@repo/eslint-config/react-internal";
+import { config } from "@axonicles/eslint-config/react-internal";
 
 /** @type {import("eslint").Linter.Config} */
 export default config;
